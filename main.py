13cf8148abffec7e175f2b2f28a754acc1127874
tomHep10 <thomasheeps10@gmail.com>
2024-12-25 23:53:19 -0500

    Created time clock and restricted  max FPS to 60

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
 
+    time = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,9 +18,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = time.tick(60) / 1000
 
 
 if __name__ == "__main__":
